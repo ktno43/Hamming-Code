222d780bcd1329e80407e4f83e91aff2072967b6
Kyle Thien Nguyen <kyle.nguyen1@yahoo.com>
2018-04-03 13:20:48 -0700

    Add files via upload

--- Source.c
+++ Source.c
@@ -1,18 +1,42 @@
+/*****************************************
+Kyle Nguyen
+COMP 222
+Spring 2018
+Prof. Robert McIlhenny
+T/R 3:30 P.M - 4:45 P.M
+
+Assignment #3: Hamming Code
+Source.c
+
+Version 3.0
+
+The program works as expected and follows
+program specifications.  Given a maximum
+length it creates a dynamic array of
+characters for the hamming string.
+Depending on the parity (even/odd) it
+will check the hamming string for any
+errors.  If an error is found the error
+bit will be displayed as well as the
+corrected hamming code.  If there is no
+error nothing will be done.
+*****************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
 
+// Function Prototypes
 void displayMenu(void);
 void setParams(void);
 void calcHamming(void);
 void freeStuff(void);
 
-/* declare global vars */
+// Global Variables
 int g_nMaxLen;
 int g_bParityOdd;
 
-/* define hamming string as a dynamic array of characters */
+// Hamming string as a dynamic array of characters
 char* g_sHammingCode = NULL;
 
 int main()
@@ -70,6 +94,7 @@ void displayMenu()
 void setParams()
 {
 	int inputFlag = 1;
+
 	// Input value for maximum length
 	printf("\nEnter the maximum length: ");
 	scanf("%d", &g_nMaxLen);
@@ -80,77 +105,75 @@ void setParams()
 		printf("Enter the parity (0 = even, 1 = odd):  ");
 		scanf("%d", &g_bParityOdd);
 
+		// If parity is valid, continue
 		if (g_bParityOdd == 1 || g_bParityOdd == 0)
 		{
 			inputFlag = 0;
 		}
 
-		else
+		else // Else invalid number
 			printf("Invalid input. \n\n");
 	}
 
-	freeStuff();
+	freeStuff(); // Be sure to free before allocating
 
-	/* allocate memory for hamming string based on maximum length and size of a character element*/
+	// Allocate memory for hamming string based on maximum length and size of char
 	g_sHammingCode = (char*)malloc(g_nMaxLen * sizeof(char));
 }
 
 void calcHamming()
 {
-	/* declare local vars */
 	int nErrorBit = 0;
 	int nHammingCodeLen = 0;
 
-	/* prompt for hamming code as a "string"*/
-	// Input value for hamming code
+	// Input string for hamming code
 	printf("\nEnter the Hamming code: ");
 	scanf("%s", g_sHammingCode);
 
-	/* Determine actual Hamming code length & number of parity bits */
+	// Determine actual Hamming code length & number of parity bits 
 	nHammingCodeLen = strlen(g_sHammingCode);
 
-	// int nNumParity = ceil(log(nHammingCodeLen)/log(2));
-	/* OUTER LOOP: for each parity bit in the Hamming code*/
+	// OUTER LOOP: for each parity bit in the Hamming code
 	int i = 0;
 	for (i = 1; i < nHammingCodeLen; i *= 2) // Parity Bit
-	{ /* BEGIN OF OUTER LOOP */
-
-	  /* initialize appropriate local variables */
+	{
 		int nParityBit = g_bParityOdd;
-		/* MIDDLE LOOP: for each starting bit of a sequence */
+
+		// MIDDLE LOOP: for each starting bit of a sequence 
 		int j = 0; // i = 1, 2, 4, 8, 16
-		for (j = i; j <= nHammingCodeLen; j = j + (2 * i))// i=1 j->2, i=2 j-> 4, i=4 j -> 12, i=8-> j->16
+		for (j = i; j <= nHammingCodeLen; j = j + (2 * i)) 
 		{
-			/* INNER LOOP: for each bit to be checked, check value and update parity information for current parity bit*/
+			// INNER LOOP: for each bit to be checked, check value and update parity information for current parity bit
 			int k = 0;
-			for (k = j; k <= nHammingCodeLen && (k < (i + j)); k++) // && something with j
-				nParityBit ^= (g_sHammingCode[nHammingCodeLen - k] - '0');
+			for (k = j; k <= nHammingCodeLen && (k < (i + j)); k++) 
+				nParityBit ^= (g_sHammingCode[nHammingCodeLen - k] - '0'); // XOR with parity bit
 		}
-		/* update parity checking by including result for parity bit checked in total */
+		// Update parity checking by including result for parity bit checked in total
 		nErrorBit += nParityBit * i; // Include weight
 	}
-	/* END OF OUTER LOOP
 
-	/* report error in hamming code based on result from parity bits or report no error if none */
+	// Report error in hamming code based on result from parity bits or report no error if none 
 
-	/* correct hamming code by flipping error bit (if necessary)*/
+	// If no error
 	if (nErrorBit == 0)
 		printf("There is no bit error\n\n");
 
-	else
+	else // Else correct hamming code by flipping error bit 
 	{
-		printf("%s%d", "There is an error in bit:  ", nErrorBit);
-		g_sHammingCode[nHammingCodeLen - nErrorBit] = ('1' + '0') - g_sHammingCode[nHammingCodeLen - nErrorBit];
-		printf("\n%s%s\n\n", "The corrected Hamming code is:  ", g_sHammingCode);
+		printf("%s%d", "There is an error in bit:  ", nErrorBit); // Display error bit
+
+		g_sHammingCode[nHammingCodeLen - nErrorBit] = ('1' + '0') - g_sHammingCode[nHammingCodeLen - nErrorBit]; // Perform the correction
+
+		printf("\n%s%s\n\n", "The corrected Hamming code is:  ", g_sHammingCode); // Display the corrected hamming code
 	}
 	return;
 }
 
 void freeStuff()
 {
-	if (g_sHammingCode != NULL)
+	if (g_sHammingCode != NULL) // When the string is not NULL
 	{
-		free(g_sHammingCode);
-		g_sHammingCode = NULL;
+		free(g_sHammingCode); // Free up memory
+		g_sHammingCode = NULL; // Reassign to NULL
 	}
 }
